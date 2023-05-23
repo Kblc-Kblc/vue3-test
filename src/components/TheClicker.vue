37979597ac6407fcfd50424c53fe899cdaf27535
Roman <kapkaevroman124@gmail.com>
2023-05-23 16:24:27 +0300

    Test Tasks

---
+++ src/components/TheClicker.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3>TheClicker</h3>
+    <button @click="$emit('count-incremented')">Click me too</button>
+  </div>
+</template>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>TheCounter</h3>
+    <slot name="default" :count="count">
+      <p>Count: {{ count }}</p>
+    </slot>
+    <button @click="$emit('count-incremented')">Click me</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  count: Number
+})
+</script>
